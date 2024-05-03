81c7ae7621dba41fc9c797cf701cb0fcf1576a17
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2024-05-03 11:41:43 +0330

    Added comments to nobody.gd
    title

--- Nobody/nobody.gd
+++ Nobody/nobody.gd
@@ -1,20 +1,25 @@
+#----------------------------------
+# Coded by Kurosh Hesamshariati
+#----------------------------------
+# thanks to Retrobright Youtube Channel for their Idea
+
 extends Sprite2D
 
 @onready var tile_map = $"../TileMap"
 @onready var sprite = $Sprite2D
 
-var is_moving = false
-var move_command = false;
+# will not let the character move towards destination unless command is given
+var move_command = false; 
 
 func _physics_process(delta):
-	if is_moving == false:
-		return
+	# will let new commands to be given once the previous is done
 	if global_position == sprite.global_position:
-		is_moving = false
 		move_command = false
 		return
+	# moving
 	if move_command == true:
 		sprite.global_position = sprite.global_position.move_toward(global_position,2)
+
 
 func _process(delta):
 	detect_input()
@@ -26,14 +31,13 @@ func detect_input():
 		return
 
 	var direction: Vector2 = Input.get_vector("left","right","up","down")
-	#if is_moving == true:
-		#return
 	
 	if direction != Vector2.ZERO:
 		move(direction)
 
 
 func move(dir: Vector2):
+	# reseting the position of the cursor
 	global_position = sprite.global_position
 	
 	var current_tile: Vector2i = tile_map.local_to_map(global_position)
@@ -42,10 +46,11 @@ func move(dir: Vector2):
 		current_tile.y + dir.y
 	)
 	
+	# i have declared a custom data layer on the Tilemap, setting some tiles 
+	# as walkable tiles. the character may only enter those tiles
 	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
 	if tile_data.get_custom_data("walkable") == false:
 		return
 	
-	is_moving = true;
 	global_position = tile_map.map_to_local(target_tile)
 	sprite.global_position = tile_map.map_to_local(current_tile)
